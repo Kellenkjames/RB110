46bc0ec932204fa1dec3fac40b10d90934331e76
Kellen James <Kellenkjames@gmail.com>
2023-03-22 16:08:26 -0400

    Solutions added for problem 4

--- practice_problems/lesson_2/sorting_collections_blocks.rb
+++ practice_problems/lesson_2/sorting_collections_blocks.rb
@@ -47,3 +47,28 @@ hsh1['b'][1]
 hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
 hsh2[:third].key(0)
 # => "g"
+
+#____________________________
+
+# Problem 4: For each of these collection objects where the value 3 occurs, demonstrate how you would change this to 4
+
+arr1 = [1, [2, 3], 4]
+arr1[1][1] = 4
+arr1
+# => [1, [2, 4], 4]
+
+arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+arr2[2] = 4
+arr2
+# => [{:a=>1}, {:b=>2, :c=>[7, 6, 5], :d=>4}, 4]
+
+hsh1 = {first: [1, 2, [3]]}
+hsh1[:first][2] = 4
+hsh1
+# => {:first=>[1, 2, 4]}
+
+hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+hsh2[['a']][:a][2] = 4
+hsh2
+# => {["a"]=>{:a=>["1", :two, 4], :b=>4}, "b"=>5}
+
