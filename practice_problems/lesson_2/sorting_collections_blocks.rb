4779cf41bb0238fc106306b4d02982bfde224e1a
Kellen James <Kellenkjames@gmail.com>
2023-03-22 17:39:27 -0400

    Add solution for problem 9

--- practice_problems/lesson_2/sorting_collections_blocks.rb
+++ practice_problems/lesson_2/sorting_collections_blocks.rb
@@ -143,3 +143,16 @@ hsh.each do |key, value|
     end
   end
 end
+
+#____________________________
+
+# Problem 9: Given this data structure, return a new array of the same structure but with the sub arrays being ordered (alphabetically or numerically as appropriate) in descending order.
+
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+arr.map do |subarray|
+  subarray.sort do |a, b|
+    b <=> a
+  end
+end
+# => [["c", "b", "a"], [3, 2, 1], ["green", "blue", "black"]]
